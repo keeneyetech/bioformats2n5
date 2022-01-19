aa106230a9638b9e3d46c7982d3c109b593343ef
ian.munro <ian.munro@keeneye.tech>
2022-01-19 14:04:44 +0100

    fix: restore original metadata

--- src/main/java/ai/keeneye/bioformats2n5/Converter.java
+++ src/main/java/ai/keeneye/bioformats2n5/Converter.java
@@ -505,7 +505,7 @@ public class Converter implements Callable<Void> {
       }
 
       // set this flag to true if original metadata is required
-      memoizer.setOriginalMetadataPopulated(false);
+      memoizer.setOriginalMetadataPopulated(true);
       memoizer.setFlattenedResolutions(false);
       memoizer.setMetadataFiltered(true);
       memoizer.setMetadataStore(createMetadata());
